b5713104ce6973e42aa98d0be17a6b6446a04c60
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-04-27 16:42:45 +0200

    Stampa 1 funzionante

--- script.js
+++ script.js
@@ -14,3 +14,42 @@ Utilizzare gli input presenti nella pagina per permettere all’utente di aggiun
 4. Permettere all'Utente di aggiungere degli oggetti (con tutte le info) all'array oggetti.
 */
 
+/***************************
+    VARIABILI GLOBALI
+***************************/
+const arrayTeam = [
+    {
+        foto: "img/wayne-barnett-founder-ceo.jpg",
+        nome: "Wayne Barnett",
+        posizione: "Founder & CEO"
+    }
+]
+console.log(arrayTeam[0].foto);
+
+
+/***************************
+    FUNZIONI DI UTILITà
+****************************/
+function stampaCollega(foto, nome, posizione){
+    const container = document.querySelector(".team-container")
+    
+    container.innerHTML += `
+        <div class="team-card">
+            <div class="card-image">
+              <img
+                src=" ${foto} "
+                alt=" ${nome} "
+              />
+            </div>
+            <div class="card-text">
+              <h3> ${nome} </h3>
+              <p> ${posizione} </p>
+            </div>
+          </div>
+    `
+}
+
+/***************************
+    INIZIALIZZAZIONE
+****************************/
+stampaCollega(arrayTeam[0].foto, arrayTeam[0].nome, arrayTeam[0].posizione);
