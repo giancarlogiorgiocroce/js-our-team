f0ce76061576d162e27961e64463241a835d6715
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-04-27 16:04:58 +0200

    Setup iniziale

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+Utilizza la card come base e poi rimuovila dall'HTML.
+File js in cui definire **un array di oggetti** in cui stampare **Nome, Ruolo e Foto**.
+Stampare dinamicamente una card per ogni membro del team.
+
+BONUS:
+Utilizzare gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team: cliccando sul pulsante “add” viene creato un **nuovo oggetto**, il quale viene **inserito nell’array iniziale** e viene stampata una nuova card con tutte le informazioni inserite dall’utente.
+*/
+
+/*
+1. Dichiarare una variabile che abbia al suo interno un array di oggetti.
+2. Estrapolare le informazioni che sono necessarie per ogni singolo oggetto (membro team).
+3. Stampare dinamicamente tutto.
+4. Permettere all'Utente di aggiungere degli oggetti (con tutte le info) all'array oggetti.
+*/
+
